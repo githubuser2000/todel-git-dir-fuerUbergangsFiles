7b1fe1d47ae9c74453770de9e46b1c51f5b45133
Alexander Kern <alexgit@alexkern.de>
2023-02-27 02:21:10 +0100

    bla

--- bruch4reta.py
+++ bruch4reta.py
@@ -5,12 +5,27 @@
 def bruchSpalt(text):
     bruchSpalten = text.split("/")
     bruchSpaltenNeu = []
-    for bS in bruchSpalten:
+    for k, bS in enumerate(bruchSpalten):
         zahl, keineZahl, bsNeu = {}, {}, []
         for i, char in enumerate(bS):
             if char.isdecimal():
                 zahl[i] = char
             else:
-                keineZahl[i] += char
+                keineZahl[i] = char
+        flag = False
+        allVergleich = [zahl > c for c, zahl in zip(keineZahl.keys(), zahl.keys())]
+        zahlSet = set(zahl.keys())
+        keineZahlSet = set(keineZahl.keys())
+        if k == 0 and all(allVergleich):
+            flag = True
+        elif k == len(bS) - 1 and not any(allVergleich):
+            flag = True
+        elif keineZahlSet.issubset(range(min(zahlSet) + 1, max(zahlSet))):
+            flag = True
+        else:
+            flag = False
+        if flag is False:
+            return []
         bsNeu = [zahl, keineZahl]
-    bruchSpaltenNeu += [bsNeu]
+        bruchSpaltenNeu += [bsNeu]
+    return bruchSpaltenNeu
