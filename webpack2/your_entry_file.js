4ee7851c9604c7fe438119488ebde15d8371c34a
Alexander Kern <alexgit@alexkern.de>
2023-04-16 13:48:37 +0200

    webpack läuft für zstd dekomprimieren mit json3

--- webpack2/your_entry_file.js
+++ webpack2/your_entry_file.js
@@ -1,4 +1,5 @@
 import {ZstdInit, ZstdDec} from '@oneidentity/zstd-js/decompress';
+import {parse} from 'json3';
 
 function loadCompressedData() {
     const base64Data = "KLUv/WD4BI0GADLIHB5QpdYBwOaJojUTL1KdYK2bHa60uyQp0yWqnsxMqw4BhurnGlvNq4qfa2w1r6p9rrHVvKr0ucZWS1YKF7+MqPEMQrEJjY9/JglJALMxONixgJ5FcYcf0MM80KI47wkaNP7uLMoMaFzAIBN29WVC0suXaRE5oCHAOEkqw54BYKVpG5gByAmRJAiCIEpmhEASBEEQJTNCIAmCkKNOSKnCVJoS6YZShwrFQ1MyZJqFlKGdHhAEtBGLUDRlByQEqAPuK3Clz7K6pC5QBbjj2gw1";
@@ -10,7 +11,10 @@ ZstdInit().then(({ZstdSimple, ZstdStream}) => {
   const someCompressedData = loadCompressedData();
 
   // Decompress the compressed simple data
-  const decompressedSimpleData = ZstdSimple.decompress(someCompressedData);
+  const decompressedSimpleData1 = ZstdSimple.decompress(someCompressedData);
+  const decompressedSimpleData2 = new TextDecoder().decode(decompressedSimpleData1);
+  const decompressedSimpleData3 = parse(decompressedSimpleData2)
 
-  console.log('[Simple]', new TextDecoder().decode(decompressedSimpleData));
+  console.log('[Simple]', decompressedSimpleData3[7]);
+  console.log('[Simple]', decompressedSimpleData3[8]);
 });
